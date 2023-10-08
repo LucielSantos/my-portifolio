5f3434740f0f31f07904892c53c46003a60a543d
Luciel Santos <luciel.vitor.bs@gmail.com>
2023-10-08 19:37:15 -0300

    fix: using dvh in main tag

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,7 +3,7 @@ import { ParticlesBackground } from "../components/ParticlesBackground";
 
 export default function HomePage() {
   return (
-    <main className="w-screen h-screen">
+    <main className="w-screen h-[100dvh]">
       <ParticlesBackground />
 
       <section className="max-w-global m-global">
